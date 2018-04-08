ea038fe340095bd052c00f2e9e4dc071243f13c2
MOTION3D <mondo@motion3d.com>
2018-04-08 00:18:06 -0700

    Adding character controller and reading some simple inputs

---
+++ BattleFreddiev3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+    public float gravityScale = 1.0f;
+
+    private Vector3 moveDirection;
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+
+
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+
+	}
+}
