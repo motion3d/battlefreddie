2f9712d66a6bb8becbd08c8c1678b3f5b0bc2608
MOTION3D <mondo@motion3d.com>
2018-04-08 16:23:10 -0700

    Adding simple touch debugs

--- BattleFreddiev3/Assets/Scripts/PlayerController.cs
+++ BattleFreddiev3/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class PlayerController : MonoBehaviour {
 
@@ -8,6 +11,8 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce;
     public CharacterController controller;
     public float gravityScale = 1.0f;
+    public Swipe swipeControls;
+    private Vector3 desiredPosition;
 
     private Vector3 moveDirection;
 
@@ -20,8 +25,32 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+
         // moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        /*
+        if (swipeControls.SwipeLeft)
+            desiredPosition += Vector3.left;
+        if (swipeControls.SwipeRight)
+            desiredPosition += Vector3.right;
+        if (swipeControls.SwipeUp)
+            desiredPosition += Vector3.forward;
+        if (swipeControls.SwipeDown)
+            desiredPosition += Vector3.back;
+        */
+
+        if (swipeControls.SwipeLeft)
+            Debug.Log("LEFT!!!");
+        if (swipeControls.SwipeRight)
+            Debug.Log("RIGHT!!!");
+        if (swipeControls.SwipeUp)
+            Debug.Log("UP!!!");
+        if (swipeControls.SwipeDown)
+            Debug.Log("DOWN!!!");
+
+        if (swipeControls.Tap)
+            Debug.Log("TAP!!!");
+
 
 
         if (controller.isGrounded)
